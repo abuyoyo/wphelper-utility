fd4013fcbb109a4bf8bb7e51576d19b51e55b2b9
abuyoyo <abuyoyo@users.noreply.github.com>
2021-04-10 16:31:27 +0300

    WordPress: wp_strtotime()
    - wp_strtotime() strtotime that uses wp_timezone()

--- functions.php
+++ functions.php
@@ -5,5 +5,6 @@
 
 require_once 'includes/polyfill_php.php';
 require_once 'includes/php.php';
+require_once 'includes/wordpress.php';
 require_once 'includes/wp_admin.php';
 require_once 'includes/utility_functions.php';
