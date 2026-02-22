97d9b58c8f1585689bd19a3b372743c1ed5c8f4d
abuyoyo <abuyoyo@users.noreply.github.com>
2026-02-22 06:01:18 +0200

    wph_admin_get_post_type + wph_get_current_admin_url
    deprecate:
    - wp_admin_get_post_type
    - wp_get_current_admin_url

--- includes/deprecated.php
+++ includes/deprecated.php
@@ -7,3 +7,5 @@ include 'deprecated/pre_print.php';
 include 'deprecated/wp_die_arr.php';
 include 'deprecated/wp_dump.php';
 include 'deprecated/wph_is_json.php';
+include 'deprecated/wp_admin_get_post_type.php';
+include 'deprecated/wp_get_current_admin_url.php';
---
+++ includes/deprecated/wp_get_current_admin_url.php
@@ -0,0 +1,26 @@
+<?php
+if ( ! function_exists( 'wp_get_current_admin_url' ) ):
+	/**
+	 * Get current admin page URL.
+	 *
+	 * Returns an empty string if it cannot generate a URL.
+	 * Based on WooCommerce wc_get_current_admin_url()
+	 * @link https://github.com/woocommerce/woocommerce/blob/master/includes/admin/wc-admin-functions.php#L506
+	 * 
+	 * @deprecated
+	 * 
+	 * @package WPHelper\Utility
+	 * 
+	 * @return string
+	 */
+	function wp_get_current_admin_url() {
+		$uri = isset( $_SERVER['REQUEST_URI'] ) ? esc_url_raw( wp_unslash( $_SERVER['REQUEST_URI'] ) ) : '';
+		$uri = preg_replace( '|^.*/wp-admin/|i', '', $uri );
+	
+		if ( ! $uri ) {
+			return '';
+		}
+	
+		return remove_query_arg( array( '_wpnonce' ), admin_url( $uri ) );
+	}
+endif;
